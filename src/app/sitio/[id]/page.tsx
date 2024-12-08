e05772f4654e07735515dc8845c5f0eae40a7be8
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-12-08 18:58:54 -0300

    fix: problem props

--- src/app/sitio/[id]/page.tsx
+++ src/app/sitio/[id]/page.tsx
@@ -1,16 +1,22 @@
 import { getSitio } from "@/actions/getSitio";
 import { Footer } from "../../footer";
-import { NextPage } from "next";
 
-const Page: NextPage<{ params: { id: string } }> = async ({ params }) => {
-    const { id } = params;
+type Props = {
+    params: { id: string };
+};
+
+export default async function Page({ params }: Props) {
+
+    const id = params.id;
 
     const site = await getSitio(id);
 
     return (
         <div className="">
+
             <main className="max-w-7xl mx-auto my-10 px-4">
                 <h1 className="text-white text-4xl font-bold mb-5">{site.name}</h1>
+
             </main>
 
             <div className="fixed bottom-0 left-0 w-full bg-gray-100 shadow-md">
@@ -18,6 +24,4 @@ const Page: NextPage<{ params: { id: string } }> = async ({ params }) => {
             </div>
         </div>
     );
-};
-
-export default Page;
+}
