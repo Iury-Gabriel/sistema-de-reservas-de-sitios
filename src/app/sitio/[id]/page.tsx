8f50fdf8150d0c24276d2e2bcaeaf7e420cc0a24
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-12-08 18:52:27 -0300

    fix sitiopageprops

--- src/app/sitio/[id]/page.tsx
+++ src/app/sitio/[id]/page.tsx
@@ -1,22 +1,21 @@
 import { getSitio } from "@/actions/getSitio";
 import { Footer } from "../../footer";
-import { Header } from "../../header";
 
+interface SitioPageProps {
+    params: {
+        id: string;
+    };
+}
 
-export default async function Page({ params }: { params: { id: string } }) {
-
+export default async function Page({ params }: SitioPageProps) {
     const { id } = params;
 
     const site = await getSitio(id);
 
-    console.log(site);
-
     return (
         <div className="">
-
             <main className="max-w-7xl mx-auto my-10 px-4">
                 <h1 className="text-white text-4xl font-bold mb-5">{site.name}</h1>
-
             </main>
 
             <div className="fixed bottom-0 left-0 w-full bg-gray-100 shadow-md">
