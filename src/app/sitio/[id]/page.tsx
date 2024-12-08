692ebac35a3d9c3b0c546aeca6c5578f8d964c11
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-12-08 19:01:24 -0300

    fix: type props

--- src/app/sitio/[id]/page.tsx
+++ src/app/sitio/[id]/page.tsx
@@ -5,18 +5,11 @@ type Props = {
     params: { id: string };
 };
 
-export default async function Page({ params }: Props) {
-
-    const id = params.id;
-
-    const site = await getSitio(id);
-
+export default function Page({ site }: { site: any }) {
     return (
         <div className="">
-
             <main className="max-w-7xl mx-auto my-10 px-4">
                 <h1 className="text-white text-4xl font-bold mb-5">{site.name}</h1>
-
             </main>
 
             <div className="fixed bottom-0 left-0 w-full bg-gray-100 shadow-md">
@@ -24,4 +17,13 @@ export default async function Page({ params }: Props) {
             </div>
         </div>
     );
+}
+
+export async function getServerSideProps(context: { params: { id: string } }) {
+    const id = context.params.id;
+    const site = await getSitio(id);
+
+    return {
+        props: { site },
+    };
 }
