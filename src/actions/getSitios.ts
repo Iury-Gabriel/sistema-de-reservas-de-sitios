98f2bb2e52b1030fbb735caad6cc525f2c230e58
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-12-18 20:20:47 -0300

    fix: reduce to 60seconds revallidaate

--- src/actions/getSitios.ts
+++ src/actions/getSitios.ts
@@ -4,21 +4,21 @@ export const getSitios = async () => {
     const sitios = await fetch('https://site-service-jbcm.onrender.com/sites', {
         cache: 'force-cache',
         next: {         
-            revalidate: 600,
+            revalidate: 60,
         },
     });
 
     const ping = await fetch('https://reservation-service-ukoi.onrender.com/ping', {
         cache: 'force-cache',
         next: {         
-            revalidate: 600,
+            revalidate: 60,
         },
     });
 
     const ping2 = await fetch('https://user-services-q0r8.onrender.com/ping', {
         cache: 'force-cache',
         next: {         
-            revalidate: 600,
+            revalidate: 60,
         },
     });
 
